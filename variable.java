f66ea243fa9cd10bd37063be7d1def1fab604096
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-07-15 13:47:02 +0530

    initial commit

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class variable{
+    public static void main(String [] args){
+        int a=10;
+        int b=20;
+        int result=a+b;
+        System.out.println(result);
+
+        char ch='v';
+        System.out.println(ch);
+
+        String name="code";
+        System.out.println(name);
+
+    }
+}
