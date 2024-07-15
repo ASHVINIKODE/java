7ebf5982eb17bf6459a5b1e4e34eac3174dbb7ba
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-07-15 13:36:22 +0530

    initial commit

---
+++ fruits.java
@@ -0,0 +1,8 @@
+public class fruits {
+    public static void main(String[] args) {
+        System.out.println("hello apple");
+        System.out.println("hello banana");
+        System.out.println("hello mango");
+        System.out.println("hello grapes");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello developers");
+        System.out.println("hello coders");
+        System.out.println("hello this is second class!");
+    }
+}
