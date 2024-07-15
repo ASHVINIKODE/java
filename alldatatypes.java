d2b4722c1e6cda9a02531809d807718f3ce4149d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-07-15 17:09:12 +0530

    initial commit

---
+++ alldatatypes.java
@@ -0,0 +1,37 @@
+public class alldatatypes {
+    public static void main(String[] args) {
+        int a=4;
+        int c=300;
+        int b=4;
+        int result=a+b+c;
+        byte b1=12;
+        float f1=10.1f;
+        double d1=10.0000001;
+        char ch='1';
+        char ch1='@';
+        char ch2='a';
+        long l1=11111111111l;
+        boolean bool1=true;
+        boolean bool2=a==b;
+        String name1="Cookie";
+        String name2="Lilly";
+        // System.out.print(a);
+        // System.out.print(b);
+        //primitive data type starts with small letter and non primitive data type starts with capital letter.
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(result);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(l1);
+        System.out.println(bool1);
+        System.out.println(bool2);
+        System.out.println(name1);
+        System.out.println(name2);
+    }
+}
