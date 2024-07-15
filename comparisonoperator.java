a8b81257dd2c384a65db98e8d805d7ea256ab581
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-07-15 22:16:52 +0530

    initial commit

---
+++ comparisonoperator.java
@@ -0,0 +1,14 @@
+public class comparisonoperator {
+    public static void main(String[] args) {
+        // comparison operators are ==,<,>,!=,<=,>=;
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+}
