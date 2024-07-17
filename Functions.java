41ac8d6a9216d724ffbfc75bb88dd828f6034485
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-07-17 21:59:12 +0530

    initial commit

---
+++ Functions.java
@@ -0,0 +1,20 @@
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // Functions f1 = new Functions();
+        // f1.getAge();
+        getAge();
+        // 
+        // 
+        // 
+        // 
+        getAge();
+    }
+
+    // public int getAge()
+    // void getAge()
+    static void getAge() {
+        System.out.println("function called");
+        // return 0;
+    }
+}
