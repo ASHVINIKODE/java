152d0ee44472d4f7a6e6133c43fb2e3e11205077
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-08-27 23:31:23 +0530

    initial commit

---
+++ FunctionsParams.java
@@ -0,0 +1,9 @@
+public class FunctionsParams {
+    public static void main(String[] args) {
+        int user1Age=28;
+        getAgeIndays(user1Age);
+    }
+    public static void getAgeIndays(int age){
+        System.out.println(age);
+    }
+}
--- alldatatypes.java
+++ alldatatypes.java
@@ -1,24 +1,25 @@
 public class alldatatypes {
     public static void main(String[] args) {
-        int a=4;
-        int c=300;
-        int b=4;
-        int result=a+b+c;
-        byte b1=12;
-        float f1=10.1f;
-        double d1=10.0000001;
-        char ch='1';
-        char ch1='@';
-        char ch2='a';
-        long l1=11111111111l;
-        boolean bool1=true;
-        boolean bool2=a==b;
-        String name1="Cookie";
-        String name2="Lilly";
+        int a = 4;
+        int c = 300;
+        int b = 4;
+        int result = a + b + c;
+        byte b1 = 12;
+        float f1 = 10.1f;
+        double d1 = 10.0000001;
+        char ch = '1';
+        char ch1 = '@';
+        char ch2 = 'a';
+        long l1 = 11111111111l;
+        boolean bool1 = true;
+        boolean bool2 = a == b;
+        String name1 = "Cookie";
+        String name2 = "Lilly";
         // System.out.print(a);
         // System.out.print(b);
-        //primitive data type starts with small letter and non primitive data type starts with capital letter.
-        //values are literal.
+        // primitive data type starts with small letter and non primitive data type
+        // starts with capital letter.
+        // values are literal.
         System.out.println(a);
         System.out.println(c);
         System.out.println(result);
