88ca29158533c2fe8278cda6b29cb4a6cc147e9d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-11-30 00:07:21 +0530

    initial commit

--- alldatatypes.java
+++ alldatatypes.java
@@ -35,5 +35,20 @@ public class alldatatypes {
         System.out.println(bool2);
         System.out.println(name1);
         System.out.println(name2);
+        int x = 15;
+        int y = 10;
+        int sum = x + y;
+        System.out.println("sum of " + x + " and " + y + " is " + sum);
+        int product = x * y;
+        System.out.println("product of " + x + " and " + y + " is " + product);
+        int v1 = x / y;
+        int v2 = y / x;
+        int v3 = x % y;
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        int exp = (x * y) / (x + y);
+        System.out.println(exp);
+
     }
 }
--- hello.java
+++ hello.java
@@ -1,7 +1,15 @@
-public class hello{
+public class hello {
     public static void main(String[] args) {
         System.out.println("hello developers");
         System.out.println("hello coders");
         System.out.println("hello this is second class!");
+        System.out.println("hello world!");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("*\n**\n***\n****\n*****");
+        System.out.println("*****\n   * \n  *  \n *   \n*****");
     }
 }
