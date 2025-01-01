d23eaf11e105689975c5cfb06ed29fb70b50c93c
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-01-01 15:43:59 +0530

    initial commit

--- InputScanner.java
+++ InputScanner.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class InputScanner {
     public static void main(String[] args) {
+
         // System.out.println("enter any number:");
-        // Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         // int a = scan.nextInt();
         // System.out.println("your entered number is:");
         // System.out.println(a);
@@ -12,15 +13,14 @@ public class InputScanner {
         // String name=scan.nextLine();
         // System.out.println("your name is:");
         // System.out.println(name);
-        System.out.println("enter your name,age,salary");
-        Scanner scan=new Scanner(System.in);
-        String name=scan.nextLine();
-        int age=scan.nextInt();
-        float salary=scan.nextFloat();
-        System.out.println("your name is:"+name);
-        System.out.println("your age is:"+age);
-        System.out.println("your salary is:"+salary);
-
+        // System.out.println("enter your name,age,salary");
+        // Scanner scan=new Scanner(System.in);
+        String name = scan.nextLine();
+        int age = scan.nextInt();
+        float salary = scan.nextFloat();
+        System.out.println("your name is:" + name);
+        System.out.println("your age is:" + age);
+        System.out.println("your salary is:" + salary);
 
     }
 }
