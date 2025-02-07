060c4f6464e9d97511bfd84a77c0bb78e61120f5
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-02-07 23:54:29 +0530

    initial commit

---
+++ revise.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class revise {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hey Ashviii this side!");
+        System.out.println(33567);
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.nextInt());
+        System.out.println(input.next());
+        // System.out.println(input.nextLine());
+        int rollNo=35;
+        char letter='a';
+        double largeDecimalNumbers=648624242.47827;
+        float marks=98.67f;
+        long largeInteger=45783242L;
+        boolean check=false;
+        System.out.println(rollNo);
+        System.out.println(letter);
+        System.out.println(largeDecimalNumbers);
+        System.out.println(marks);
+        System.out.println(largeInteger);
+        System.out.println(check);
+    }
+}
